457e2672140a08f0906c75962b89f17c16da636d
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-05-21 16:18:14 +0200

    update arcjet lib

--- server/lib/arcjet.js
+++ server/lib/arcjet.js
@@ -27,9 +27,9 @@ export const aj = arcjet({
     // Create a token bucket rate limit. Other algorithms are supported.
     tokenBucket({
       mode: "LIVE", //Her 10 saniyede bir 5 token eklenir max capacity is 10, 10 dan fazla token bulunmaz.
-      refillRate: 5, // Refill 5 tokens per interval 
-      interval: 10, // Refill every 10 seconds
-      capacity: 10, // Bucket capacity of 10 tokens
+      refillRate: 30, // Refill 5 tokens per interval 
+      interval: 5, // Refill every 10 seconds
+      capacity: 20, // Bucket capacity of 10 tokens
     }),
   ],
 });
